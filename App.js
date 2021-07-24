d50aeab1df66a62d341c9535c7340964de828059
gunwoongPark <olympiodoros@naver.com>
2021-07-24 20:22:04 +0900

    Asking for Permissions

--- App.js
+++ App.js
@@ -1,15 +1,25 @@
 import React, { useState, useEffect } from "react";
+import { Alert } from "react-native";
 import Loading from "./Loading";
 import * as Location from "expo-location";
 
 export default function App() {
-  const [location, setLocation] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(async () => {
-    const newLocation = await Location.getCurrentPositionAsync();
-    setLocation(newLocation);
-    console.log(location);
-  });
+  useEffect(() => {
+    async function getLocation() {
+      try {
+        await Location.requestForegroundPermissionsAsync();
+        const {
+          coords: { latitude, longitude },
+        } = await Location.getCurrentPositionAsync();
+        setIsLoading(false);
+      } catch (error) {
+        Alert.alert("Can't find you.", "So sad");
+      }
+    }
+    getLocation();
+  }, []);
 
-  return <Loading />;
+  return isLoading ? <Loading /> : null;
 }
