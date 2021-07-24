317a8299b300aa60b2a06e1089c047b594fe898e
gunwoongPark <olympiodoros@naver.com>
2021-07-24 17:13:21 +0900

    Getting the Location

--- App.js
+++ App.js
@@ -1,7 +1,15 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Loading from "./Loading";
+import * as Location from "expo-location";
 
 export default function App() {
+  const [location, setLocation] = useState(null);
+
+  useEffect(async () => {
+    const newLocation = await Location.getCurrentPositionAsync();
+    setLocation(newLocation);
+    console.log(location);
+  });
+
   return <Loading />;
 }
